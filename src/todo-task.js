5bf09e0ed608f178c68cbb2601ac076c22e98c2e
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-01-18 12:20:25 +0500

    Add a class for creating todos

---
+++ src/todo-task.js
@@ -0,0 +1,10 @@
+class TodoTask {
+    constructor(title, description, due_date, priority){
+        this.title = title;
+        this.description = description;
+        this.due_date = due_date;
+        this.priority = priority;
+    }
+}
+
+export default TodoTask;
